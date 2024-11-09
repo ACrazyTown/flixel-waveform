9e35e0bcd7cf6431f90efa256711d52e17b8dde7
ACrazyTown <luigibros626@gmail.com>
2024-11-09 23:09:22 +0100

    Documentation for sample

--- samples/basic/source/PlayState.hx
+++ samples/basic/source/PlayState.hx
@@ -23,19 +23,31 @@ class PlayState extends FlxState
 		FlxG.sound.music.stop();
 		FlxG.sound.music.looped = true;
 
-		// How far can we stretch the sound before hitting bitmap max limits?
+		// Check if bitmap max texture size is available.
 		if (FlxG.bitmap.maxTextureSize != -1)
+		{
+			// Calculate how far can we stretch the waveform before hitting bitmap max limits?
 			pixelsPerMs = Math.ceil(FlxG.sound.music.length / FlxG.bitmap.maxTextureSize);
+		}
 		else
-			pixelsPerMs = 16; // Random number as fallback
+		{
+			// In case we don't have a hardware accelerated renderer, we can't accurately
+			// get a maximum texture size, so let's just arbitrarily set it to a random number.
+			pixelsPerMs = 16;
+		}
 
+		// Create a new FlxWaveform instance.
 		waveform = new FlxWaveform(0, 0, Std.int(FlxG.sound.music.length / pixelsPerMs), FlxG.height, 0xFF1C98E0);
+		// Load data from the FlxSound so the waveform renderer can process it.
 		waveform.loadDataFromFlxSound(FlxG.sound.music);
+		// We set our draw range.
+		// When we leave it blank it'll default to a range from the beginning to the full length of the sound.
 		waveform.setDrawRange();
+		// We'll render both channels of the waveform seperately.
 		waveform.waveformDrawMode = SPLIT_CHANNELS;
-		// We do it once here, and if we change any properties the bitmap will be automatically
-		// updated because `waveform.autoUpdateBitmap` is true by default.
-		waveform.generateWaveformBitmap();
+		// We don't have to manually generate the bitmap here, because `FlxWaveform.autoUpdateBitmap`
+		// is true by default, and changing the waveform draw mode above will trigger a redraw.
+		// waveform.generateWaveformBitmap();
 		add(waveform);
 
 		FlxG.sound.music.play(true);
@@ -47,6 +59,7 @@ class PlayState extends FlxState
 
 		if (FlxG.sound.music.playing)
 		{
+			// Make our camera follow the audio time.
 			camera.scroll.x = FlxG.sound.music.time / pixelsPerMs;
 		}
 
