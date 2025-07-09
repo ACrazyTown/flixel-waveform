0d980817f52a5af76a682e72854524a35bd6a480
ACrazyTown <ACrazyTown@users.noreply.github.com>
2025-07-09 12:58:23 +0000

    deploy: 745bb898251ab5598a8fce50088a588ce6f46a20

--- sample-playground.js
+++ sample-playground.js
@@ -99592,7 +99592,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 332996;
+	this.version = 239715;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
